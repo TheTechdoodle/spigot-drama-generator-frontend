f209e8ce9bea9805e497d69308d741f380e089ab
TheTechdoodle <admin+github@darkender.com>
2020-07-01 16:51:25 -0700

    add github and data source

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,11 @@
         <v-app-bar app color="primary" dark>
             <v-toolbar-title class="headline">
                 <span class="font-weight-light">Spigot Drama Generator</span>
+                <v-btn icon
+                       href="https://github.com/TheTechdoodle/spigot-drama-generator-frontend"
+                >
+                    <v-icon>mdi-github</v-icon>
+                </v-btn>
             </v-toolbar-title>
             <v-spacer></v-spacer>
             <dark-switch/>
@@ -13,6 +18,19 @@
                 <v-row class="text-center">
                     <v-col cols="12">
                         <random-generator/>
+                    </v-col>
+                </v-row>
+                <v-row class="text-center">
+                    <v-col cols="12">
+                        <v-card>
+                            <v-card-text class="text-subtitle-1">
+                                <span class="text-decoration-line-through">Steals</span>
+                                 Uses data from
+                                <a href="https://github.com/md678685/spigot-drama-generator">
+                                    md678685's Spigot Drama Generator
+                                </a>
+                            </v-card-text>
+                        </v-card>
                     </v-col>
                 </v-row>
             </v-container>
