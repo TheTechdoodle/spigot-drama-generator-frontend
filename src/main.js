1c092ba4a8510ffd8e77d332dce7256102f210bb
TheTechdoodle <admin+github@darkender.com>
2020-07-01 14:03:57 -0700

    add dark switch and page app bar

--- src/main.js
+++ src/main.js
@@ -1,23 +1,35 @@
 import Vue from 'vue';
 import App from './App.vue';
 import loadData from './dataLoader';
-import vuetify from './plugins/vuetify';
+import Vuetify from 'vuetify/lib';
+import localforage from 'localforage';
+
+localforage.config({
+    name: 'spigot-drama-generator-frontend'
+});
 
 Vue.config.productionTip = false;
+Vue.use(Vuetify);
 
-(async () =>
+(async() =>
 {
+    const theme = await localforage.getItem('theme');
     let dramaData = null;
     try
     {
         dramaData = await loadData();
     }
-    catch(e) {}
+    catch(e)
+    {
+    }
     Vue.prototype.$dramaData = dramaData;
     
     new Vue({
-        vuetify,
-
+        vuetify: new Vuetify({
+            theme: {
+                dark: theme !== 'light'
+            }
+        }),
         render: function(h)
         {
             return h(App);
